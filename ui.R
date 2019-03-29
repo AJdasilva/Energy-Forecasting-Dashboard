b16f2ee550e9539ab8da407cba16fa5a0ed52bc6
Aaron da Silva <36546012+AJdasilva@users.noreply.github.com>
2019-03-28 20:20:20 -0400

    Made updates to style of dashboard
    added background color to power consumption, changed fluidity in terms of the dashboard being responsive to changing window size

--- ui.R
+++ ui.R
@@ -21,7 +21,7 @@ time_period = "late evening"
 peak_time = "yes"
 
 # Set up the application ui
-shinyUI(navbarPage("Forecasting Dashboard",
+shinyUI(navbarPage("Forecasting Dashboard", fluid = "TRUE",
                    
                    # define the tabs to be used in the app ----------------------------------------
                    tabPanel("Dashboard Instructions",
@@ -32,31 +32,38 @@ shinyUI(navbarPage("Forecasting Dashboard",
                    # POWER CONSUMPTION TAB
                    tabPanel("Power Consumption",
                             # set up styling for map
-                            div(class="outer",
+                            div(class="power",
                                 tags$head(
                                   includeCSS("./styles.css"))),
                             fluidRow(
-                              box(title = "Power Consumption Forecasting")
+                              box(title = "Power Consumption Forecasting", width = 12)
                             ),
                             # Boxes need to be put in a row (or column)
                             fluidRow(
-                              box("Error of Use Model (mse):",textOutput("text1"))
+                              box("Error of Use Model (mse):",
+                                  textOutput("text1")),
+                              width = 12
+                            ),
+                            br(),
+                            fluidRow(
+                                 box(
+                                     width = 8,
+                                     plotOutput("plot1", width = '100%')
+                                     ),
+                                 box(
+                                    width = 4,
+                                    sliderInput("slider", "Forecast Horizon (days)", 1, 50, 7, width = '100%'),
+                                    dateRangeInput("date_range", "Analysis Range: Start Date to End Date", start = start_date, end = end_date, width = '100%'),
+                                    dateInput("train_date","Date to End Training",value = train_end_date, width = '100%'),
+                                    selectInput(width = '100%', "time_period", "Choose time period:", 
+                                                choices = c('Entire Day'='whole_day','Early Morning 1:00am-8:00am'='early morning','Afternoon 8:00am-11:00am'='morning',
+                                                            'Late Afternoon noon-6:00pm'='afternoon',
+                                                            'Evening 6:00pm-8:00pm'='evening','Late Evening 8:00pm-midnight'='late evening')),
+                                    dateRangeInput(width = '100%', "window_range", "Viewing Window Range: Start Date to End Date", start = view_window_start, end = view_window_end)
+                                 )
                             ),
                             fluidRow(
-                              
-                              box(plotOutput("plot1", height = 400)),
-                              box(
-                                title = "Controls",
-                                sliderInput("slider", "Forecast Horizon (days)", 1, 50, 7),
-                                dateRangeInput("date_range", "Analysis Range: Start Date to End Date", start = start_date, end = end_date),
-                                dateInput("train_date","Date to End Training",value = train_end_date),
-                                selectInput("time_period", "Choose time period:", 
-                                            choices = c('Entire Day'='whole_day','Early Morning 1:00am-8:00am'='early morning','Afternoon 8:00am-11:00am'='morning',
-                                                        'Late Afternoon noon-6:00pm'='afternoon',
-                                                        'Evening 6:00pm-8:00pm'='evening','Late Evening 8:00pm-midnight'='late evening')),
-                                dateRangeInput("window_range", "Viewing Window Range: Start Date to End Date", start = view_window_start, end = view_window_end)
-                              ),
-                              box(dataTableOutput('table', width = 850))
+                                box(dataTableOutput('table'),width = 12)
                             ),
                             hr()),
                    # SOLAR POWER GENERATION TAB
@@ -66,25 +73,28 @@ shinyUI(navbarPage("Forecasting Dashboard",
                                 tags$head(
                                   includeCSS("./styles.css"))),
                             fluidRow(
-                              box(title = "Solar Power Generation Forecasting")
+                              box(title = "Solar Power Generation Forecasting", width = 12)
                             ),
                             # Boxes need to be put in a row (or column)
                             fluidRow(
                               box("Error of Use Model (mse):",textOutput("text2"))
                             ),
+                            br(),
                             fluidRow(
                               
-                              box(plotOutput("plot2", height = 400)),
+                              box(plotOutput("plot2"), width = 8),
                               box(
-                                title = "Controls",
-                                sliderInput("solslide", "Forecast Horizon (days)", 1, 50, 3),
-                                dateRangeInput("sol_date_range", "Analysis Range: Start Date to End Date", start = start_date, end = end_date),
-                                dateInput("sol_train_date","Date to End Training",value = train_end_date),
-                                selectInput("peak_time", "Choose time period:", 
+                                width = 4,
+                                sliderInput(width = '100%', "solslide", "Forecast Horizon (days)", 1, 50, 3),
+                                dateRangeInput(width = '100%', "sol_date_range", "Analysis Range: Start Date to End Date", start = start_date, end = end_date),
+                                dateInput(width = '100%', "sol_train_date","Date to End Training",value = train_end_date),
+                                selectInput(width = '100%', "peak_time", "Choose time period:", 
                                             choices = c('Peak Solar 9:00am-5:00pm'='yes','Non-Peak Solar 5:00pm-9:00am'='no')),
-                                dateRangeInput("sol_win_range", "Viewing Window Range: Start Date to End Date", start = view_window_start, end = view_window_end)
-                              ),
-                              box(dataTableOutput('table2', width = 850))
+                                dateRangeInput(width = '100%', "sol_win_range", "Viewing Window Range: Start Date to End Date", start = view_window_start, end = view_window_end)
+                              )
+                            ),
+                            fluidRow(
+                              box(dataTableOutput('table2'), width = 12)
                             ),
                             hr())
                    
